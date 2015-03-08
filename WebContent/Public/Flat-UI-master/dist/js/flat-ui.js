44fefbc8877e0922cc5fa280401d6fa4f92efaa8
harveyprince <948841233@qq.com>
2015-03-09 02:34:20 +0800

    server page detail construct----film plan

--- WebContent/Public/Flat-UI-master/dist/js/flat-ui.js
+++ WebContent/Public/Flat-UI-master/dist/js/flat-ui.js
@@ -4867,7 +4867,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
-
+/*
 +function ($) {
   'use strict';
 
@@ -5139,7 +5139,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   })
 
 }(jQuery);
-
+*/
 /* ========================================================================
  * Bootstrap: tooltip.js v3.2.0
  * http://getbootstrap.com/javascript/#tooltip
