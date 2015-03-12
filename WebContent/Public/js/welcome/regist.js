171d7cda5e4566d8f94861e30aa95c3edafe6898
harveyprince <948841233@qq.com>
2015-03-12 23:46:07 +0800

    regist page

---
+++ WebContent/Public/js/welcome/regist.js
@@ -0,0 +1,7 @@
+$("select.sex-input").select2();
+$(".info-regist-button").click(function(){
+	$(".password-tab").click();
+});
+$(".password-regist-button").click(function(){
+	$(".result-tab").click();
+});
